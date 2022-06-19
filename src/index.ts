17a8804651ae99fbec26f54944ee85e7637cc080
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-06-19 21:21:43 +0300

    Add event handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { User } from '../models/User';
 
 const user = new User({ name: 'Eleri', age: 27 });
-user.set({ name: 'Ellurix' });
-console.log(user.get('name'));
-console.log(user.get('age'));
+// user.set({ name: 'Ellurix' });
+// console.log(user.get('name'));
+// console.log(user.get('age'));
+
+user.on('change', () => {
+  console.log('je');
+});
+
+user.on('change', () => {
+  console.log('ja');
+});
+
+user.trigger('change');
