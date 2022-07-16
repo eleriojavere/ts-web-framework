1d939bf8bd509a23675390e20d784facbbd8bdbf
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-07-16 23:16:10 +0300

    Implement compositon by moving Eventing into separate model

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { User } from '../models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'Eleri', age: 27 });
 
-user.set({ name: 'Ellurixx' });
 user.save();
+user.events.on('change', () => {
+  console.log('jau');
+});
+
+user.events.trigger('change');
