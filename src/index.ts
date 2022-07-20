9c07bd373f25556b02f25e1dcc33a5d896d6ed9f
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-07-20 14:57:34 +0300

    Change get, trigger, on  method to arrow function to bound this keyword correctly

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { User } from '../models/User';
 
 const user = new User({ name: 'Eleri', age: 27 });
 
-user.on('change', () => {
-  console.log('jau');
-});
+console.log(user.get('age'));
+
+// user.on('change', () => {
+//   console.log('jau');
+// });
