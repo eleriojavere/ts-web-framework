9ecb4e0563f26f4282a1710b6d23b7d645059f71
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-07-21 16:56:05 +0300

    Add buildUserCollection method to User class

--- models/User.ts
+++ models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
   id?: number;
@@ -14,6 +15,10 @@ const rootUrl = 'http://localhost:3000/users';
 export class User extends Model<UserProps> {
   static buildUser(attributes: UserProps): User {
     return new User(new Attributes<UserProps>(attributes), new Eventing(), new ApiSync<UserProps>(rootUrl));
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(`${rootUrl}`, (json: UserProps) => User.buildUser(json));
   }
 
   isAdminUser(): boolean {
