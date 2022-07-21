bf58d9b8e51813ac39913a46758f4e75c0324e2f
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-07-21 14:57:23 +0300

    Add save method to User class that triggers save

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from '../models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ id: 1, name: 'newer nameeeh', age: 21 });
 
 user.on('change', () => {
   console.log(user);
 });
 
-user.fetch();
+user.save();
