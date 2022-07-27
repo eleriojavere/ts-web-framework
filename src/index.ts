74d48b68cfc6b5c02824bfccbb22b7685ca785bc
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-07-27 13:05:17 +0300

    Add name changing logic and type guards to avoid strict type errors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'Eleri', age: 20 });
 
-const userForm = new UserForm(document.getElementById('root'), user);
-
-userForm.render();
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('No root element found');
+}
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,17 +14,32 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
   };
 
   template(): string {
-    return `<div><h1>User form</h1><div>name: ${this.model.get('name')} <div>age: ${this.model.get(
-      'age'
-    )}</div></div><input/><button>Click me</button><button class="set-age">Set random age</button></div>`;
+    return `<div>
+    <h1>User form</h1>
+    <div>name: ${this.model.get('name')} </div>
+    <div>age: ${this.model.get('age')}</div>
+    <input/>
+    <button class='set-name'>Change name</button>
+    <button class="set-age">Set random age</button>
+    </div>`;
   }
 
   bindEvents(fragment: DocumentFragment): void {
