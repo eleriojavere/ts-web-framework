216acb791366c664819ac3c5e05d8368e7189015
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-07-30 18:51:25 +0300

    Map regions and render them

--- src/views/UserShow.ts
+++ src/views/UserShow.ts
@@ -5,8 +5,8 @@ export class UserShow extends View<User, UserProps> {
   template(): string {
     return `<div>
     <h1>User detail</h1>
-    <div>User name ${this.model.get('name')}</div>
-    <div>User age ${this.model.get('age')}</div>
+    <div>User name: ${this.model.get('name')}</div>
+    <div>User age: ${this.model.get('age')}</div>
     </div>`;
   }
 }
