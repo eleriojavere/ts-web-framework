d091050ae361de9bafe6a2c376ea4fe084a5e23b
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-07-30 19:17:38 +0300

    Add collection view class and rendering

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { User } from './models/User';
-import { UserEdit } from './views/UserEdit';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const user = User.buildUser({ name: 'Eleri', age: 20 });
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json);
+});
 
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('No root element found');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
